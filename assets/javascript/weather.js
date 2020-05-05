6e2cb9054a3f3cb26020811ed014c6042567137a
Ryan Skog <ryan.skog@outlook.com>
2020-05-05 10:14:07 -0700

    Added a lot of javascript dealing with pulling the API and sorting results

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -5,7 +5,9 @@ $("#search-button").on("click", function () {
 
     let citySearch = $("#search-city").val().trim();
     let apiKey = "462e1590393042b6de4c11d6437c183d";
+    let altAPI = "d98dcd8fe5c2fc97c49fcf8a578e2735";
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
+    let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid" + altAPI;
 
     $.ajax({
         url: queryURL,
@@ -14,8 +16,13 @@ $("#search-button").on("click", function () {
         .then(function (response) {
 
             let city = response.name;
+            let conditions = response.weather[0].main;
+
+            console.log(response);
+            console.log(conditions);
 
             cityHistory.push(city);
+            localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
 
             if (cityHistory.length > 7) {
                 historyDiv.lastElementChild.remove();
@@ -29,21 +36,41 @@ $("#search-button").on("click", function () {
                 $("#city-history").prepend(newCity);
             }
 
-            $(".temp-top-text").text("The current temperature in " + city + " is:");
+            $("#today-image").attr("src", "./assets/images/stormy.jpg");
 
             // Converts Kelvin to Fahrenheit
             let tempF = parseInt((response.main.temp - 273.15) * 1.80 + 32);
-            $("#fTemp").text(tempF);
+            $("#fTemp").html(tempF + "<span>&#176</span>");
+
+            // Populates current day weather details
+            $(".temp-top-text").text("The current temperature in " + city + " is:");
+            $("#humidity").html("Humidity: " + response.main.humidity + "<span>&#37</span>");
+            $("#windSpeed").text("Wind Speed: " + response.wind.speed + " mph");
+            $("#uvIndex").text("UV Index: Placeholder");
+            $("#weather-list").append(humidity, windSpeed, uvIndex);
 
 
             // Clears input field
             $("#search-city").val("");
         }) // End AJAX call
+
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            console.log(response);
+
+        })
+
+
+
 }) // End search button function
 
-$("#city-button").on("click", function () {
+$(".city-button").on("click", function () {
 
-    let city = $(this).attr("id");
+    let citySearch = $(this).attr("id");
     console.log(city);
 
 })
