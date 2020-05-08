6de4cc54b11b37c9f0a73faa68249044be7bc7cc
Ryan Skog <ryan.skog@outlook.com>
2020-05-07 18:42:24 -0700

    Fixed history button issue, created new functions for accessability

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -2,54 +2,71 @@ let historyDiv = document.getElementById("city-history");
 let cityHistory = [];
 let apiKey = "462e1590393042b6de4c11d6437c183d";
 
+$(document).ready(function () {
+
+    if (window.localStorage.length == 0) {
+    } else {
+        cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+
+        for (i = 0; i < cityHistory.length; i++) {
+            let city = cityHistory[i];
+            let newCity = $("<button>").text(city).addClass("btn btn-info city-button")
+                .attr("id", city);
+            $("#city-history").append(newCity);
+        }
+    }
+})
+
 $("#search-button").on("click", function () {
     let citySearch = $("#search-city").val().trim();
     getWeather(citySearch);
+    makeButton();
 })
+
+$("body").on("click", ".city-button", function () {
+    getWeather($(this).attr("id"));
+})
+
+function makeButton() {
+
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+    city = cityHistory[0];
+
+    if (cityHistory.length > 7) {
+        historyDiv.lastElementChild.remove();
+        var newCity = $("<button>").text(city).addClass("btn btn-info city-button")
+            .attr("id", city);
+        $("#city-history").prepend(newCity);
+        cityHistory.splice(0, 1);
+    } else {
+        var newCity = $("<button>").text(city).addClass("btn btn-info city-button")
+            .attr("id", city);
+        $("#city-history").prepend(newCity);
+    }
+}
 
 function getWeather(city) {
 
-    // This is a nightmare.  Without the unbind method, it pulls multiple times depending on 
-    // what location that element is in if I were to array the buttons.  Without the .city-button
-    // as a parameter, it will not pull from the first button since array location would be 0.
-    $("#city-history").unbind().on("click", ".city-button", function (event) {
-        event.preventDefault();
-        getWeather($(this).attr("id"));
-        // $.when(ajax1()).done(function () {
-        //     historyDiv.firstElementChild.remove();
-        //     cityHistory.splice(0, 1);
-        // })
-    });
-
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-
+    // This call gets coordinates of the city to use in the One Call API in OpenWeather
     $.ajax({
         url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
+        method: "GET",
+        async: false,
+        error: function () {
+            alert("This is not a city.");
+        },
+        success: (function (response) {
             const city = response.name;
             const latitude = response.coord.lat;
             const longitude = response.coord.lon;
             const forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat="
                 + latitude + "&lon=" + longitude + "&exclude=hourly&appid=" + apiKey;
 
-            if (cityHistory.length > 6) {
-                historyDiv.lastElementChild.remove();
-                var newCity = $("<button>").text(city).addClass("btn btn-info city-button")
-                    .attr("id", city);
-                $("#city-history").prepend(newCity);
-                cityHistory.splice(0, 1);
-            } else {
-                var newCity = $("<button>").text(city).addClass("btn btn-info city-button")
-                    .attr("id", city);
-                $("#city-history").prepend(newCity);
-            }
+            cityHistory.unshift(city);
 
-            cityHistory.push(city);
-            localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
-
+            // This One Call API populates all weather detail by converting coordinates to a city
             $.ajax({
                 url: forecastURL,
                 method: "GET"
@@ -99,25 +116,10 @@ function getWeather(city) {
                     // Clears input field
                     $("#search-city").val("");
 
-                }) // End AJAX call
-        })
-}
-
-$(document).ready(function () {
-
-    if (window.localStorage.length == 0) {
-    } else {
-        cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
-
-        for (i = 0; i < cityHistory.length; i++) {
-            let city = cityHistory[i];
-            let newCity = $("<button>").text(city).addClass("btn btn-info city-button")
-                .attr("id", city);
-            $("#city-history").prepend(newCity);
-        }
-    }
-    getWeather(cityHistory[0]);
-})
+                }); // End second AJAX call
+        }) // End success function of first AJAX call
+    }) // End first AJAX call
+} // End getWeather function
 
 function convertF(number) {
     tempF = parseInt((number - 273.15) * 1.80 + 32);
